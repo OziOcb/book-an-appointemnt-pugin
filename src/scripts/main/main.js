157ba20014f3cbd5a0454aa606691560e4b22905
Paul <paulozyzniewski@bigfootdigital.co.uk>
2019-06-26 16:13:44 +0100

    Create plugin's base view

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -1,9 +1,38 @@
-class SomeClass {
-	method() {
-		console.log("I'm here!");
-	}
+$(document).ready(function() {
+	// tab wizard
+	$('a[data-toggle="tab"]').on("show.bs.tab", function(e) {
+		var $target = $(e.relatedTarget);
+		if ($target.parent().hasClass("disabled")) {
+			return false;
+		}
+	});
+
+	$(".next-step").click(function(e) {
+		var $active = $(".nav-tabs .nav-link.active");
+		$active
+			.parent()
+			.next()
+			.find(".nav-link")
+			.removeClass("disabled");
+		nextTab($active);
+	});
+	$(".prev-step").click(function(e) {
+		var $active = $(".nav-tabs li>a.active");
+		prevTab($active);
+	});
+});
+
+function nextTab(elem) {
+	$(elem)
+		.parent()
+		.next()
+		.find('a[data-toggle="tab"]')
+		.click();
 }
-
-const test = new SomeClass();
-
-test.method();
+function prevTab(elem) {
+	$(elem)
+		.parent()
+		.prev()
+		.find('a[data-toggle="tab"]')
+		.click();
+}
