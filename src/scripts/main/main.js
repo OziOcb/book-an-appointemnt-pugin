6dd30a6adda464bb0b9cce3b9cae3fe454b552a2
Paul <paulozyzniewski@bigfootdigital.co.uk>
2019-07-16 13:01:50 +0100

    Add a basic form validation (prevents from passing empty inputs only)

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -9,9 +9,11 @@ const prevBtns = document.querySelectorAll(".prev-step");
 
 // go to the Next Step & remove .disabled form the Next Tab
 const goToNextStep = e => {
+	// First validate all step's inputs
 	const step = parseInt(e.target.dataset.step);
-	const nextTab = document.querySelector(`#tabStep${step + 1}`);
+	if (!validateForm(step)) return false;
 
+	const nextTab = document.querySelector(`#tabStep${step + 1}`);
 	nextTab.classList.remove("disabled");
 	nextTab.click();
 };
@@ -20,6 +22,29 @@ const goToNextStep = e => {
 const goToPrevStep = e => {
 	const step = parseInt(e.target.dataset.step);
 	const prevTab = document.querySelector(`#tabStep${step - 1}`);
-
 	prevTab.click();
 };
+
+// Do the validation
+const myForm = document.querySelector("#stepped");
+const steps = myForm.querySelectorAll(".tab-pane");
+
+function validateForm(step) {
+	const input = steps[step - 1].querySelectorAll(".form-control");
+	let valid = true;
+
+	for (let i = 0; i < input.length; i++) {
+		if (input[i].value == "") {
+			if (!input[i].classList.contains("invalid")) {
+				input[i].classList.add("invalid");
+			}
+			valid = false;
+		}
+		if (!input[i].value == "") {
+			if (input[i].classList.contains("invalid")) {
+				input[i].classList.remove("invalid");
+			}
+		}
+	}
+	return valid;
+}
